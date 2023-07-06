0a0e2092e2a57e139ebbbde41d7b5dd179c17d2b
Alvin Yu <alvinyu2003@gmail.com>
2023-07-06 12:58:32 -0700

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Alvin Yu on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
